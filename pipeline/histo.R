d5206bcf37d5e7f0550f7ee9a2be71c67aab95b0
Gian Nico <gian.benucci@gmail.com>
2022-07-26 12:26:47 -0400

    Delete pipeline directory

--- pipeline/histo.R
+++
@@ -1,6 +0,0 @@
-reads<-read.csv(file="readhisto.txt", sep="", header=FALSE)
-pdf(file="plothisto.pdf", width=5, height=10) 
-plot(reads$V2,reads$V1,type="l",xlab="read length",ylab="occurences",col="red")
-dev.off()
-
-
